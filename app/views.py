b42dce26a8b9684f939fd3e5400a2952aa29dd79
Jose Jaime Come <josejaimecome@gmail.com>
2024-01-06 22:16:34 +0200

    home_view and add_new_post_view added

--- app/views.py
+++ app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.forms import PostForm
+from app.models import Post
+from datetime import datetime
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_view(request):
+    form = PostForm()
+    Posts = Post.objects.all()
+    return render(
+        request, 'home.html', 
+        {'form': form, 'posts': Posts}
+)
+
+
+def add_new_post_view(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            #form.save()
+            try:
+                form = form.save(commit=False)
+                form.user_id = 1
+                form.date_created = datetime.now()
+                form.save()
+                return redirect('/')
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            pass
+    else:
+        return redirect('/')
+
+    
