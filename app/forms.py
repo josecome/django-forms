a6d8cb5c47de9d7e91032fccc3f151840b2e4dd6
Jose Jaime Come <josejaimecome@gmail.com>
2024-01-06 22:13:43 +0200

    models and forms added

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from app.models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'post_content']
+
+
--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    post_content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateField()
+    date_updated = models.DateField(null=True)
+
+    @property
+    def username(self):
+        return self.user.username
+
+    class Meta:  
+        db_table = "posts"      
